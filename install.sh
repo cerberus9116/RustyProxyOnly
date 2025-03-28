ac9391731b01510d81beaf3b2b39a331d8d5c3b2
OneVp <96548634+WorldSsh@users.noreply.github.com>
2025-03-27 21:26:00 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -26,7 +26,7 @@ else
     echo -e "\033[0;32m█▀█ █░█ █▀ ▀█▀ █▄█  █▀█ █▀█ █▀█ ▀▄▀ █▄█
 █▀▄ █▄█ ▄█ ░█░ ░█░  █▀▀ █▀▄ █▄█ █░█ ░█░"
     echo -e " "
-    echo -e "             DEV:@𝗨𝗟𝗘𝗞𝗕𝗥   EDIÇÃO:@𝐉𝐄𝐅𝐅𝐒𝐒𝐇"
+    echo -e "          DEV:@𝗨𝗟𝗘𝗞𝗕𝗥   EDIÇÃO:@𝐉𝐄𝐅𝐅𝐒𝐒𝐇"
     echo -e " "
     show_progress "ATUALIZANDO REPOSITÓRIO..."
     export DEBIAN_FRONTEND=noninteractive
