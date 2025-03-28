49b763668577b728afc4488a35c98e3be1748684
JeffSsh <96548634+WorldSsh@users.noreply.github.com>
2025-03-28 16:32:02 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -126,6 +126,6 @@ echo -e "\033[0;34m-------------------------------------------------------------
 echo -e "\E[44;1;37m            INSTALAÇÃO FINALIZADA COM SUCESSO                 \E[0m"
 echo -e "\033[0;34m--------------------------------------------------------------\033[0m"
 echo -e " "
-echo -e "\033[1;31m \033[1;33mCOMANDO PARA ACESSAR O MENU: \033[1;32mrustyproxy\033[0m"
+echo -e "\033[1;31m \033[1;33mDIGITE O COMANDO PARA ACESSAR O MENU: \033[1;32mrustyproxy\033[0m"
 echo -e " "
 fi
